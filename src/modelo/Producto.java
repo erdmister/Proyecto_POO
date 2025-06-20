272ead5276add834a89b78d531b2115c92c50f63
dnaxruz <dan_uchiha@comunidad.unam.mx>
2025-06-19 22:16:29 -0600

    feat: productoDao consulta base de datos, crear Productos

---
+++ src/modelo/Producto.java
@@ -0,0 +1,86 @@
+package modelo;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private double costo;
+    private int stock;
+    private Date fechaCaducidad;
+
+    // Constructor
+    public Producto(int id, String nombre, double precio, double costo, int stock, Date fechaCaducidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.costo = costo;
+        this.stock = stock;
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Date getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public void setFechaCaducidad(Date fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", costo=" + costo +
+                ", stock=" + stock +
+                ", fechaCaducidad=" + fechaCaducidad +
+                '}';
+    }
+}
